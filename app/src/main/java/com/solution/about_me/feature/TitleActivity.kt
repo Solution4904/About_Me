e2d779eaa0e91cbe33391607cc54d63d8f75c923
Solution4904 <shinjseoup@gmail.com>
2024-05-21 16:52:44 +0900

    complete xml (prototype)

--- app/src/main/java/com/solution/about_me/feature/TitleActivity.kt
+++ app/src/main/java/com/solution/about_me/feature/TitleActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.solution.about_me.databinding.ActivityTitleBinding
 
 class TitleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,11 @@ class TitleActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        init()
+    }
+
+    private fun init() {
+        val binding = DataBindingUtil.setContentView<ActivityTitleBinding>(this, R.layout.activity_title)
     }
 }
